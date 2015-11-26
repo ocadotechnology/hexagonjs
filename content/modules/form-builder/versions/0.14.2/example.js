098f3022a827bb8f0c81870576c739cc8bc84528
James Smyth <james.smyth@ocado.com>
2015-11-26 16:48:13 +0000

    fix references to hx-icon back to fa. hx-icon is not part of the public api

--- content/modules/form-builder/versions/0.14.2/example.js
+++ content/modules/form-builder/versions/0.14.2/example.js
@@ -1,9 +1,9 @@
 var form = new hx.Form('#form')
-  .addText('Text', { required:true, placeholder:'Name' })
-  .addTextArea('Text Area', { placeholder:'Name' })
-  .addEmail('Email', { required:true, placeholder:'your.name@ocado.com' })
-  .addUrl('Url', { placeholder:'http://www.example.co.uk/' }) // Allows blank or valid URL (with http:// prefix)
-  .addNumber('Number', {required:true})
+  .addText('Text', { required: true, placeholder: 'Name' })
+  .addTextArea('Text Area', { placeholder: 'Name' })
+  .addEmail('Email', { required: true, placeholder: 'your.name@ocado.com' })
+  .addUrl('Url', { placeholder: 'http://www.example.co.uk/' }) // Allows blank or valid URL (with http:// prefix)
+  .addNumber('Number', {required: true})
   .addPicker('Select', ['red', 'green', 'blue'])
   .addCheckbox('Checkbox')
   .addPassword('Password')
@@ -12,10 +12,10 @@ var form = new hx.Form('#form')
   .addDatePicker('Date Picker')
   .addTimePicker('Time Picker')
   .addDateTimePicker('Date Time Picker')
-  .addSubmit('Submit', 'hx-icon hx-icon-check')
-  .on('submit', function(data){console.log(data)});
+  .addSubmit('Submit', 'fa fa-check')
+  .on('submit', function (data) {console.log(data)})
 
 var radioNode = hx.select(form.properties.get('Radio').node)
-radioNode.selectAll('input').on('click', function(e) {
-  console.log( form.property('Radio') );
+radioNode.selectAll('input').on('click', function (e) {
+  console.log(form.property('Radio'))
 })
