93b9702bdb0c30653d23b53a8537b362dcf0969f
James Smyth <james.smyth@ocado.com>
2015-11-30 10:33:04 +0000

    better naming for options

--- build/main/demo.js
+++ build/main/demo.js
@@ -22,31 +22,36 @@ var builder = require('./builder')
 var path = require('path')
 var liveServer = require('live-server')
 var gaze = require('gaze')
+var merge = require('merge')
 
-module.exports = function (options) {
-  options = options || {}
-  var port = options.port || 9009
-  var dest = options.dest || 'demo'
-  var startServer = !options.noServer
+module.exports = function (opts) {
+  var options = merge({
+    port: 9009,
+    dest: 'demo',
+    serverEnabled: true,
+    watchEnabled: true
+  }, opts)
 
   var demoDir = path.join(__dirname, '..', '..', 'demo')
 
-  fs.copyAsync(demoDir, dest)
+  fs.copyAsync(demoDir, options.dest)
 
-  if (startServer) {
+  if (options.watchEnabled) {
     gaze(path.join(demoDir, '**', '*'), function (err, watcher) {
       if (err) {
         console.error(err)
       } else {
         watcher.on('changed', function () {
-          fs.copyAsync(demoDir, dest)
+          fs.copyAsync(demoDir, options.dest)
         })
       }
     })
+  }
 
+  if (options.serverEnabled) {
     liveServer.start({
-      port: port, // Set the server port. Defaults to 8080.
-      root: dest, // Set root directory that's being server. Defaults to cwd.
+      port: options.port, // Set the server port. Defaults to 8080.
+      root: options.dest, // Set root directory that's being server. Defaults to cwd.
       open: false, // When false, it won't load your browser by default.
       wait: 0 // Waits for all changes, before reloading. Defaults to 0 sec.
     })
