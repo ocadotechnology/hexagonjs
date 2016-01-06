a6b6e1dae4de2d15798f9f70450545aa6b209422
Charlie Frater <charlie.frater@ocado.com>
2016-01-06 11:57:52 +0000

    move print css/js to asset files, update to include changes from comments and update the build config to include hexagon.print.css and hexagon.print.js as assets

--- themes/hexagon-light/build.js
+++ themes/hexagon-light/build.js
@@ -5,12 +5,6 @@ var favicons = require('../favicon.js')
 module.exports = hexagon
   .theme(path.join(__dirname, 'theme.um'))
   .assets({
-    'logo.svg': {
-      filepath: path.join(__dirname, '../assets/hexagon-logo-v1.svg'),
-      allowEmbed: true
-    }
-  })
-  .assets({
     'assets/open-sans-light.woff': {
       filepath: path.join(__dirname, '../assets/open-sans-light.woff'),
       allowEmbed: true
