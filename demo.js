0b7e075368fa0e8fc68d239a1674bd9ccafc1ec0
j.schuchart <j.schuchart@ocado.com>
2016-08-12 17:58:34 +0100

    added hexagon.dark.watch back into demo.js

--- demo.js
+++ demo.js
@@ -3,9 +3,9 @@ var liveServer = require('live-server')
 
 hexagon.light.watch({ dest: 'target/hexagon-light' })
 hexagon.demo({ dest: 'target/hexagon-light', serverEnabled: false })
-//
-// hexagon.dark.watch({ dest: 'target/hexagon-dark' })
-// hexagon.demo({ dest: 'target/hexagon-dark', serverEnabled: false })
+
+hexagon.dark.watch({ dest: 'target/hexagon-dark' })
+hexagon.demo({ dest: 'target/hexagon-dark', serverEnabled: false })
 
 liveServer.start({
   port: 9009,
