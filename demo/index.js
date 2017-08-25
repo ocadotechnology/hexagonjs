948dfc893012cdef0564c159841ca2db6b917972
Charlie Frater <charlie.frater@ocado.com>
2017-08-25 15:44:01 +0100

    create a demo page that has all the css / css + js components that can be used to easily demo theme changes

--- demo/index.js
+++ demo/index.js
@@ -50,9 +50,46 @@ new hx.Picker('#viewMenu', {
   }
 })
   .on('change', function (p) {
-    hx.select('#container').class('hx-group hx-horizontal view-' + p.value.text)
-      .selectAll('.exampleSection')
-      .class('hx-section exampleSection ' + p.value.value)
-      .selectAll('.example')
-      .class('example ' + (p.value.contentClass || ''))
+    if (p.value.contentClass) {
+      hx.selectAll('.demo-section')
+        .class(`demo-section ${p.value.value}`)
+        .select('.demo-section-examples')
+          .class(`demo-section-examples ${p.value.contentClass || ''}`)
+    } else {
+      hx.selectAll('.demo-section')
+        .class('demo-section')
+        .select('.demo-section-examples')
+          .class(`demo-section-examples ${p.value.value || ''}`)
+    }
   })
+
+const contents = hx.select('#contents')
+
+function slug (text) {
+  return text.toLowerCase().split(' ').join('-')
+}
+
+if (contents.size()) {
+  contents.append(hx.selectAll('.demo-group').map(sel => {
+    const titleSel = sel.select('h2')
+    const titleText = titleSel.text()
+    const titleSlug = slug(titleText)
+    titleSel.attr('id', titleSlug)
+    return hx.detached('li')
+      .add(hx.detached('a')
+        .attr('href', `#${titleSlug}`)
+        .text(titleText))
+      .add(hx.detached('ul')
+        .add(sel.selectAll('.demo-section').map(innerSel => {
+          const titleSel = innerSel.select('h3')
+          const titleText = titleSel.text()
+          const titleSlug = slug(titleText)
+          titleSel.attr('id', titleSlug)
+          return hx.detached('li')
+            .add(hx.detached('a')
+              .attr('href', `#${titleSlug}`)
+              .text(titleText))
+        })))
+  }))
+  new hx.Collapsible('#contents-collapsible')
+}
