6dd8d6f34d262a94935c15460cf2ed4aae478b59
James Smyth <james.smyth@ocado.com>
2018-07-04 13:47:52 +0100

    Port time picker tests

--- modules/time-picker/spec.js
+++ modules/time-picker/spec.js
@@ -1,31 +1,39 @@
 import { TimePicker } from 'time-picker'
 import { div } from 'selection'
 
-export default () ->
-  describe 'time-picker', ->
-    describe 'api', ->
-      it 'hour', ->
-        tp = new TimePicker(div())
+export default () => {
+  describe('time-picker', () => {
+    describe('api', () => {
+      it('hour', () => {
+        const tp = new TimePicker(div())
         tp.hour(5).should.equal(tp)
         tp.hour().should.equal(5)
+      })
 
-      it 'minute', ->
-        tp = new TimePicker(div())
+      it('minute', () => {
+        const tp = new TimePicker(div())
         tp.minute(5).should.equal(tp)
         tp.minute().should.equal(5)
+      })
 
-      it 'second', ->
-        tp = new TimePicker(div())
+      it('second', () => {
+        const tp = new TimePicker(div())
         tp.second(5).should.equal(tp)
         tp.second().should.equal(5)
+      })
 
-      it 'date', ->
-        tp = new TimePicker(div())
-        date = new Date
+      it('date', () => {
+        const tp = new TimePicker(div())
+        const date = new Date()
         tp.date(date).should.equal(tp)
         tp.date().should.eql(date)
+      })
 
-      it 'locale', ->
-        tp = new TimePicker(div())
+      it('locale', () => {
+        const tp = new TimePicker(div())
         tp.locale('en-GB').should.equal(tp)
         tp.locale().should.equal('en-GB')
+      })
+    })
+  })
+}
