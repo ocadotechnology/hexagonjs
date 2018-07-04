794236b632b6d544ce5b03719af67e79acba63fd
James Smyth <james.smyth@ocado.com>
2018-07-04 14:37:46 +0100

    Port the pivot table tests

--- modules/pivot-table/spec.js
+++ modules/pivot-table/spec.js
@@ -3,24 +3,16 @@ import { PivotTable } from 'pivot-table'
 import { div } from 'selection'
 import chai from 'chai'
 
+export default () => {
+  const should = chai.should()
 
-export default () ->
-  should = chai.should()
+  describe('Pivot Table', () => {
+    it('should have the correct default options', () => {
+      const topHead = range(2)
+      const leftHead = range(2)
+      const body = range(2).map(x => range(2).map(y => x * y))
 
-  describe 'Pivot Table', ->
-    it 'should have the correct default options', ->
-      topHead = range(2)
-      leftHead = range(2)
-      body = range(2).map (x) ->
-        range(2).map (y) -> x * y
-
-      data = {
-        body: body,
-        leftHead: leftHead,
-        topHead: topHead
-      }
-
-      pt = new PivotTable(div())
+      const pt = new PivotTable(div())
 
       pt.options.stickyHeaders.should.equal(true)
       should.not.exist(pt.options.topLeftCellRender)
@@ -28,69 +20,72 @@ export default () ->
       pt.options.useResponsive.should.equal(true)
       should.not.exist(pt.options.data)
       should.not.exist(pt.options.fullWidth)
+    })
 
-    it 'should not mutate the data', ->
-      topHead = range(8)
-      leftHead = range(8)
-      body = range(8).map (x) ->
-        range(8).map (y) -> x * y
+    it('should not mutate the data', () => {
+      const topHead = range(8)
+      const leftHead = range(8)
+      const body = range(8).map(x => range(8).map(y => x * y))
 
-      data = {
-        body: body,
-        leftHead: leftHead,
-        topHead: topHead
+      const data = {
+        body,
+        leftHead,
+        topHead
       }
 
-      pt = new PivotTable(div(), {
-        data: data
+      const pt = new PivotTable(div(), {
+        data
       })
-      topHead.should.eql([0,1,2,3,4,5,6,7])
-      leftHead.should.eql([0,1,2,3,4,5,6,7])
+      topHead.should.eql([0, 1, 2, 3, 4, 5, 6, 7])
+      leftHead.should.eql([0, 1, 2, 3, 4, 5, 6, 7])
 
       pt.data().should.not.equal(data)
       pt.data().should.eql(data)
       pt.data(pt.data()).should.equal(pt)
+    })
 
-    it 'should pass the fullWidth option through to the sticky table', ->
-      topHead = range(2)
-      leftHead = range(2)
-      body = range(2).map (x) ->
-        range(2).map (y) -> x * y
+    it('should pass the fullWidth option through to the sticky table', () => {
+      const topHead = range(2)
+      const leftHead = range(2)
+      const body = range(2).map(x => range(2).map(y => x * y))
 
-      data = {
-        body: body,
-        leftHead: leftHead,
-        topHead: topHead
+      const data = {
+        body,
+        leftHead,
+        topHead
       }
 
-      pt = new PivotTable(div(), {
-        data: data
+      const pt = new PivotTable(div(), {
+        data,
         fullWidth: true
       })
 
       pt.options.fullWidth.should.equal(true)
       pt.stickyTableHeaders._.options.fullWidth.should.equal(true)
+    })
 
-    it 'should use the fullWidth option when sticky tables are disabled', ->
-      topHead = range(2)
-      leftHead = range(2)
-      body = range(2).map (x) ->
-        range(2).map (y) -> x * y
+    it('should use the fullWidth option when sticky tables are disabled', () => {
+      const topHead = range(2)
+      const leftHead = range(2)
+      const body = range(2).map(x => range(2).map(y => x * y))
 
-      data = {
-        body: body,
-        leftHead: leftHead,
-        topHead: topHead
+      const data = {
+        body,
+        leftHead,
+        topHead
       }
 
-      sel = div()
+      const sel = div()
 
-      pt = new PivotTable(sel.node(), {
-        data: data
-        fullWidth: true
+      const pt = new PivotTable(sel.node(), {
+        data,
+        fullWidth: true,
         stickyHeaders: false
       })
 
       pt.options.fullWidth.should.equal(true)
       should.not.exist(pt.stickyTableHeaders)
       sel.selectAll('.hx-table-full').size().should.equal(1)
+    })
+  })
+}
