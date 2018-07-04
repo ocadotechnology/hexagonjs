08f30eb3a3abc3a83ca765ed3f0dea3cf56d24fe
James Smyth <james.smyth@ocado.com>
2018-07-04 13:37:12 +0100

    Convert autocomplete tests to es6

--- modules/autocomplete/spec.js
+++ modules/autocomplete/spec.js
@@ -1,42 +1,78 @@
-import { userFacingText } from 'user-facing-text'
-import { Autocomplete } from 'autocomplete'
-import { range } from 'utils'
-import { select, div } from 'selection'
-import { config as dropdownConfig } from 'dropdown'
+import { userFacingText } from 'user-facing-text/main'
+import { Autocomplete } from 'autocomplete/main'
+import { range } from 'utils/main'
+import { select, div } from 'selection/main'
+import { config as dropdownConfig } from 'dropdown/main'
 
-export default () ->
-  describe 'autocomplete', ->
-    fixture = select('body').append(div('hx-test-autocomplete'))
+export default () => {
+  describe('autocomplete', () => {
+    const fixture = div('hx-test-autocomplete')
+    select('body').add(fixture)
 
-    beforeEach ->
+    after(() => {
+      fixture.remove()
+    })
+
+    beforeEach(() => {
       dropdownConfig.attachToSelector = fixture
+    })
 
-    afterEach ->
+    afterEach(() => {
       dropdownConfig.attachToSelector = 'body'
       fixture.clear()
+    })
 
-    after ->
-      fixture.remove()
+    it('should have user facing text defined', () => {
+      userFacingText('autocomplete', 'loading').should.equal('Loading...')
+      userFacingText('autocomplete', 'noResultsFound').should.equal('No results found')
+      userFacingText('autocomplete', 'otherResults').should.equal('Other Results')
+      userFacingText('autocomplete', 'pleaseEnterMinCharacters').should.equal('Please enter $minLength or more characters')
+    })
 
-    it 'should have user facing text defined', ->
-      userFacingText('autocomplete','loading').should.equal('Loading...')
-      userFacingText('autocomplete','noResultsFound').should.equal('No results found')
-      userFacingText('autocomplete','otherResults').should.equal('Other Results')
-      userFacingText('autocomplete','pleaseEnterMinCharacters').should.equal('Please enter $minLength or more characters')
+    it('should sort items correctly when using objects', () => {
+      const selection = div()
+      fixture.add(selection)
 
-    it 'should sort items correctly when using objects', ->
-      itemsObjects = range(100).map((i) -> { id: i })
+      const items = range(100).map(id => ({ id }))
 
-      ac = new Autocomplete(fixture.append(div()), itemsObjects, { inputMap: ({id}) -> id })
-
-      ac.show()
-      ac._.menu.items().map(({id}) -> id).should.eql(range(100))
-
-    it 'should sort disabled items correctly', ->
-      itemsObjects = range(100).map((i) -> { id: i, disabled: i % 2 == 0 })
-
-      ac = new Autocomplete(fixture.append(div()), itemsObjects, { inputMap: ({id}) -> id })
+      const ac = new Autocomplete(selection, items, {
+        inputMap: ({id}) => id
+      })
 
       ac.show()
-      ac._.menu.items().slice(0, 50).map(({id}) -> id).should.eql(range(50).map (i) -> 2 * i + 1)
-      ac._.menu.items().slice(50, 100).map(({id}) -> id).should.eql(range(50).map (i) -> 2 * i)
+      ac._.menu
+        .items()
+        .map(({id}) => id)
+        .should.eql(range(100))
+    })
+
+    it('should sort disabled items correctly', () => {
+      const selection = div()
+      fixture.add(selection)
+
+      const items = range(100).map(i => {
+        return {
+          id: i,
+          disabled: (i % 2) === 0
+        }
+      })
+
+      const ac = new Autocomplete(selection, items, {
+        inputMap: ({id}) => id
+      })
+
+      ac.show()
+      ac._.menu
+        .items()
+        .slice(0, 50)
+        .map(({id}) => id)
+        .should.eql(range(50).map(i => (2 * i) + 1))
+
+      ac._.menu
+        .items()
+        .slice(50, 100)
+        .map(({id}) => id)
+        .should.eql(range(50).map(i => 2 * i))
+    })
+  })
+}
