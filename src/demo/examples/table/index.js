1face4c33adb52912da8a0bedb7ff0a6d60b6632
charlie.frater <charlie.frater@ocado.com>
2019-06-20 14:24:43 +0100

    feat: Tweak data table to support table flag and make flag table full width

--- src/demo/examples/table/index.js
+++ src/demo/examples/table/index.js
@@ -1,15 +1,21 @@
-import { div, detached } from 'hexagon-js';
+import { div, detached, notify } from 'hexagon-js';
+
 const note = () => detached('note');
 
 export default () => {
-  function row(n) {
-    return detached('tr')
+  function row(n, clickable) {
+    const r = detached('tr');
+    if (clickable) {
+      r.on('click', () => notify(`Row ${n} clicked!`));
+    }
+
+    return r
       .add(detached('td').text(`Row ${n} Cell 1`))
       .add(detached('td').text(`Row ${n} Cell 2`))
       .add(detached('td').text(`Row ${n} Cell 3`));
   }
 
-  function table() {
+  function table(clickable) {
     return detached('table').class('hx-table')
       .add(detached('thead')
         .add(detached('tr')
@@ -17,18 +23,18 @@ export default () => {
           .add(detached('th').text('Header 2'))
           .add(detached('th').text('Header 3'))))
       .add(detached('tbody')
-        .add(row(1))
-        .add(row(2))
-        .add(row(3))
-        .add(row(4))
-        .add(row(5)));
+        .add(row(1, clickable))
+        .add(row(2, clickable))
+        .add(row(3, clickable))
+        .add(row(4, clickable))
+        .add(row(5, clickable)));
   }
 
   return div().set([
     table(),
     note().text('Table with feature flag to apply font size and other style changes'),
     table().classed('hx-flag-table', true),
-    note().text('Table with feature hoverable rows'),
-    table().classed('hx-flag-table hx-table-clickable-rows', true),
+    note().text('Table with feature clickable rows'),
+    table(true).classed('hx-flag-table hx-table-clickable-rows', true),
   ]);
 };
