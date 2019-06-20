0c162222aecd7c99d2c7f5602ea31d37d73839b8
charlie.frater <charlie.frater@ocado.com>
2019-06-20 14:09:56 +0100

    feat: Add feature flag for table styles

--- src/demo/examples/table/index.js
+++ src/demo/examples/table/index.js
@@ -1,4 +1,5 @@
-import { detached } from 'hexagon-js';
+import { div, detached } from 'hexagon-js';
+const note = () => detached('note');
 
 export default () => {
   function row(n) {
@@ -8,16 +9,26 @@ export default () => {
       .add(detached('td').text(`Row ${n} Cell 3`));
   }
 
-  return detached('table').class('hx-table')
-    .add(detached('thead')
-      .add(detached('tr')
-        .add(detached('th').text('Header 1'))
-        .add(detached('th').text('Header 2'))
-        .add(detached('th').text('Header 3'))))
-    .add(detached('tbody')
-      .add(row(1))
-      .add(row(2))
-      .add(row(3))
-      .add(row(4))
-      .add(row(5)));
+  function table() {
+    return detached('table').class('hx-table')
+      .add(detached('thead')
+        .add(detached('tr')
+          .add(detached('th').text('Header 1'))
+          .add(detached('th').text('Header 2'))
+          .add(detached('th').text('Header 3'))))
+      .add(detached('tbody')
+        .add(row(1))
+        .add(row(2))
+        .add(row(3))
+        .add(row(4))
+        .add(row(5)));
+  }
+
+  return div().set([
+    table(),
+    note().text('Table with feature flag to apply font size and other style changes'),
+    table().classed('hx-flag-table', true),
+    note().text('Table with feature hoverable rows'),
+    table().classed('hx-flag-table hx-table-clickable-rows', true),
+  ]);
 };
