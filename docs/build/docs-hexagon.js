61c78e2119f74abcb1855c32728bb00fceca42d5
charlie.frater <charlie.frater@ocado.com>
2019-06-26 10:41:29 +0100

    chore: Update postinstall to prevent inconsistent versions

--- docs/build/docs-hexagon.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-
-const hexJs = fs.readFileSync('resources/hexagon/docs/hexagon.js', 'utf8');
-fs.writeFileSync('resources/hexagon/docs/hexagon.js', hexJs.replace('var hx', 'var dx'));
